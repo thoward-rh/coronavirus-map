d50758bb16c852f4c892a71c02a36b8d6543b33d
Tim Howard <thoward@redhat.com>
2020-06-12 00:02:21 +0700

    Final ready for test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import axios from 'axios';
+import L from 'leaflet';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
@@ -13,7 +14,6 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-
 
 const IndexPage = () => {
 
@@ -28,14 +28,87 @@ const IndexPage = () => {
 
       let response;
 
-    try {
-      response = await axios.get('https://corona.lmao.ninja/v2/countries');
-    } catch(e) {
-      console.log(`Failed to fetch countries: ${e.message}`, e);
-      return;
+      try {
+        response = await axios.get('https://corona.lmao.ninja/v2/countries');
+      } catch(e) {
+        console.log(`Failed to fetch countries: ${e.message}`, e);
+        return;
+      }
+
+      const { data = [] } = response;
+      const hasData = Array.isArray(data) && data.length > 0;
+    
+    if ( !hasData ) return;
+    
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+           ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        }
+      })
     }
 
-    const { data = [] } = response;
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+    
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+    
+        casesString = `${cases}`;
+    
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+    
+        if ( updated ) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+    
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+    
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+
+    geoJsonLayers.addTo(map)
+  
   }
 
   const mapSettings = {
@@ -44,6 +117,9 @@ const IndexPage = () => {
     zoom: DEFAULT_ZOOM,
     mapEffect,
   };
+
+  
+
 
   return (
     <Layout pageName="home">
